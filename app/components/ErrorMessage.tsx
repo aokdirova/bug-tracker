85289eb8ea41b48cecb7a389e111457c031c5752
Aygul Okdirova <aygul.okdirova@check24.de>
2023-11-11 14:56:25 +0100

    extract error message component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren } from "react";
+import { Text } from "@radix-ui/themes";
+
+const ErrorMessage: FC<PropsWithChildren> = ({ children }) => {
+  if (!children) return null;
+  return (
+    <Text color='red' as='p'>
+      {children}
+    </Text>
+  );
+};
+
+export default ErrorMessage;
